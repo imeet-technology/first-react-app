f094a274a23cbf0a0e8594017594c075a3a99dc6
Krishna-sm <krishna.web18@gmail.com>
2023-06-17 20:49:47 +0530

    done

---
+++ src/App2.js
@@ -0,0 +1,14 @@
+import { Day, Day1  } from "./App";
+
+
+function App2(){
+    return (
+        <>
+                <Day1/>
+                <Day/>
+                APp2
+        </>
+    );
+}
+
+export default App2;
